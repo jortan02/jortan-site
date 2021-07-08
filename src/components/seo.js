099b03cf1300a2dec16bfa3b482d24b2bbde7b15
Nullindrome <jortan2002@gmail.com>
2021-07-08 17:01:25 -0600

    Removed .env

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
+import { getImage } from "gatsby-plugin-image";
 import { useStaticQuery, graphql } from "gatsby";
 
 // https://strapi.io/blog/build-a-static-blog-with-gatsby-and-strapi
@@ -14,6 +15,11 @@ const Seo = ({ seo = {} }) => {
     const fullSeo = { ...defaultSeo, ...seo };
 
     const getMetaTags = () => {
+        console.log(fullSeo);
+        console.log(fullSeo.shareImage.localFile.publicUR);
+
+        console.log(getImage(fullSeo.shareImage.localFile));
+
         const tags = [];
 
         if (fullSeo.metaDescription) {
@@ -26,9 +32,10 @@ const Seo = ({ seo = {} }) => {
         }
 
         if (fullSeo.shareImage) {
+            const image = getImage(fullSeo.shareImage.localFile);
             const imageUrl =
                 (process.env.GATSBY_ROOT_URL || "http://localhost:8000") +
-                fullSeo.shareImage.localFile.publicURL;
+                image;
             tags.push(
                 {
                     name: "image",
